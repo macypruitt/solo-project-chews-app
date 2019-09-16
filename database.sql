13ee4e66c86fba54b0fcc9ea23405aedc4ef79b2
Macy Pruitt <macypruitt@gmail.com>
2019-09-16 11:48:18 -0500

    created database chews_app, added to pool, created users and restaurants tables

--- database.sql
+++ database.sql
@@ -8,3 +8,25 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "users" (
+"id" serial primary key,
+"username" varchar(20) not null,
+"email" varchar(30) not null,
+"security level" int
+"password" varchar(30) not null);
+
+CREATE TABLE "restaurants" (
+"id" serial primary key,
+"name" varchar(30) not null,
+"address" varchar(60) not null,
+"phone" varchar(15) not null,
+"description" varchar(500) not null,
+"website" varchar(30) not null,
+"keto" boolean,
+"gluten_free" boolean,
+"vegan" boolean,
+"submitted" date,
+"approved" boolean,
+"lat" int,
+"lng" int);
