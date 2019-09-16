bf0e47884e985b5e47fbdd0d66326fe78c5b2556
Macy Pruitt <macypruitt@gmail.com>
2019-09-16 14:23:02 -0500

    made GET route to query database for all approved restaurants

---
+++ src/components/MapPage/MapPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+
+
+export class MapPage extends Component {
+  state={}
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}
+      >
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+       
+      </Map>
+    );
+  }
+}
+ 
+export default GoogleApiWrapper({
+  apiKey: (process.env.GOOGLE_MAPS)
+})(MapPage)
