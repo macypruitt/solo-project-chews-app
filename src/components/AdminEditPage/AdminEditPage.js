031b3a56cd0cd6d653817d7102747ae0cf1bf4dd
Macy Pruitt <macypruitt@gmail.com>
2019-09-19 16:49:57 -0500

    cleaned up form inputs on AdminEditPage

--- src/components/AdminEditPage/AdminEditPage.js
+++ src/components/AdminEditPage/AdminEditPage.js
@@ -26,49 +26,61 @@ class AdminEditPage extends Component {
     render() {
         console.log(this.state);
         return (
-            <div>
+            <div className="content-container">
                 <h2>Edit Listing</h2>
 
                 <div className="edit-box">
-                    <p>{this.state.name}</p>
-                    <p></p>
-                    {this.props.store.editReducer[0].name}
+                    <p>Name: {this.props.store.editReducer[0].name}</p>
                     <input type="text"
-                        onChange={(event) => this.handleChangeInputText(event, 'name')}>
+                        onChange={(event) => this.handleChangeInputText(event, 'name')}
+                        placeholder="New name">
                     </input>
                 </div>
 
-                <br></br>
-                {this.props.store.editReducer[0].address}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'address')}>
-                </input>
-                <br></br>
-                {this.props.store.editReducer[0].phone}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'phone')}>
-                </input>
-                <br></br>
-                {this.props.store.editReducer[0].description}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'description')}>
-                </input>
-                <br></br>
-                {this.props.store.editReducer[0].website}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'website')}>
-                </input>
+                <div className="edit-box">
+                    <p>Address: {this.props.store.editReducer[0].address}</p>
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'address')}>
+                    </input>
+                </div>
+                
+                <div className="edit-box">
+                    <p>Phone: {this.props.store.editReducer[0].phone}</p>
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'phone')}>
+                    </input>
+                </div>
+                
+                <div className="edit-box">
+                    <p>Description: {this.props.store.editReducer[0].description}</p>
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'description')}>
+                    </input>
+                </div>
+                
+                <div className="edit-box">
+                    <p>Website:  {this.props.store.editReducer[0].website}</p>
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'website')}>
+                    </input>
+                </div>
 
+                <div className="edit-box">
+                    <p>Lattitude: {this.props.store.editReducer[0].lat}</p>
+                    
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'lat')}>
+                    </input>
+                </div>
+                
+                <div className="edit-box">
+                    <p>Longitude: {this.props.store.editReducer[0].lng}</p>
+                    <input type="text"
+                        onChange={(event) => this.handleChangeInputText(event, 'lng')}>
+                    </input>
+                </div>
                 <br></br>
-                {this.props.store.editReducer[0].lat}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'lat')}>
-                </input>
-                <br></br>
-                {this.props.store.editReducer[0].lng}
-                <input type="text"
-                    onChange={(event) => this.handleChangeInputText(event, 'lng')}>
-                </input>
+                <button>Save</button>
             </div>
         );
     }
