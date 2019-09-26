c7707abde130af0813327d5b38aca5f8a5416bf7
Macy Pruitt <macypruitt@gmail.com>
2019-09-26 11:14:42 -0500

    added silverware icon to modal

--- src/components/MapPage/Modal.js
+++ src/components/MapPage/Modal.js
@@ -4,6 +4,7 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import { isTemplateElement } from '@babel/types';
 import compass from '../MapPage/compass.svg'
 import Button from '@material-ui/core/Button'
+import Silverware from './restaurant.svg'
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace 
@@ -19,10 +20,14 @@ class Modal extends Component {
                 <Button size="small" className="btn-exit-modal" onClick={this.props.modalToggle}>X</Button>
 
                 <div className="modal-content">
+                    
                     <h3 className="modal-title">{this.props.store.modalReducer.name}</h3>
+                    
                     {this.props.store.modalReducer.address}
                     {this.props.store.modalReducer.phone}
-                    
+                    <br />
+                    <br />
+                    <img src={Silverware} className="silverware"></img><br></br>
                     <p>{this.props.store.modalReducer.description}</p>
                     <br></br>
                     
